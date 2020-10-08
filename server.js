e108f4c6022f5328fdcb94362e1405f1058415c8
Ann-Marie <anniedillier@gmail.com>
2020-10-07 19:02:23 -0700

    updates candidates

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ const db = new sqlite3.Database("./db/election.db", (err) => {
 
 // Get all candidates
 app.get("/api/candidates", (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
   const params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
@@ -37,47 +41,24 @@ app.get("/api/candidates", (req, res) => {
   });
 });
 
-// GET a single candidate
+// Get single candidate
 app.get("/api/candidate/:id", (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id 
+                WHERE candidates.id = ?`;
   const params = [req.params.id];
   db.get(sql, params, (err, row) => {
     if (err) {
       res.status(400).json({ error: err.message });
       return;
     }
+
     res.json({
       message: "success",
       data: row,
-    });
-  });
-});
-
-// // Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//               VALUES (?,?,?,?)`;
-// const params = [1, "Ronald", "Firbank", 1];
-// // ES5 function, not arrow function, to use this
-// db.run(sql, params, function (err, result) {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result, this.lastID);
-// });
-
-// Delete a candidate
-app.delete("/api/candidate/:id", (req, res) => {
-  const sql = `DELETE FROM candidates WHERE id = ?`;
-  const params = [req.params.id];
-  db.run(sql, params, function (err, result) {
-    if (err) {
-      res.status(400).json({ error: res.message });
-      return;
-    }
-
-    res.json({
-      message: "successfully deleted",
-      changes: this.changes,
     });
   });
 });
@@ -107,6 +88,23 @@ app.post("/api/candidate", ({ body }, res) => {
       message: "success",
       data: body,
       id: this.lastID,
+    });
+  });
+});
+
+// Delete a candidate
+app.delete("/api/candidate/:id", (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+
+    res.json({
+      message: "successfully deleted",
+      changes: this.changes,
     });
   });
 });
